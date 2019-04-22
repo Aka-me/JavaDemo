4af784d1238afe870c5d91af8cd92e3941e52e4d
Aka-me <35109707+Aka-me@users.noreply.github.com>
2019-04-22 23:28:38 +0800

    add Shell Sort

--- src/pers/akame/sort/ShellSort.java
+++ src/pers/akame/sort/ShellSort.java
@@ -1,4 +1,22 @@
 package pers.akame.sort;
 
-public class ShellSort {
+public class ShellSort extends Sort {
+    public static void sort(int[] sequence){
+        int len=sequence.length;
+        int temp,gap=len/2;
+        while (gap>0){
+            for (int i = gap; i < len; i++) {
+                temp=sequence[i];
+                int preIndex=i-gap;
+                while (preIndex>=0&&sequence[preIndex]>temp){
+                    //由于前面都是已经排完序的,所以这里while条件不满足可以直接退出
+                    //这里实际上用的插入排序
+                    sequence[preIndex+gap]=sequence[preIndex];
+                    preIndex-=gap;
+                }
+                sequence[preIndex+gap]=temp;
+            }
+            gap/=2;
+        }
+    }
 }
